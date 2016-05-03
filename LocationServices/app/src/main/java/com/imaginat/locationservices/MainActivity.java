1877343e34c030969c18c27db488dd377545cfe2
programmingnat <np7771@yahoo.com>
2016-05-03 12:30:41 -0400

    Added basic functionality to get coordinates using google geolocation, and fused. Main obstacle was that forgot to add and call loadPermissions method before it started working

--- LocationServices/app/src/main/java/com/imaginat/locationservices/MainActivity.java
+++ LocationServices/app/src/main/java/com/imaginat/locationservices/MainActivity.java
@@ -1,13 +1,98 @@
 package com.imaginat.locationservices;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
+public class MainActivity extends AppCompatActivity
+ implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    public static final String TAG = MainActivity.class.getName();
+    private static final int REQUEST_FINE_LOCATION = 0;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate about to call mGoogleApiClient.Builder");
+         mGoogleApiClient = new GoogleApiClient.Builder(this)
+        .enableAutoManage(this /* FragmentActivity */,
+        this /* OnConnectionFailedListener */)
+                 .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+        if (mGoogleApiClient != null) {
+            Log.d(TAG,"googleApiClient is not null, attempting to connect");
+            mGoogleApiClient.connect();
+        }else{
+            Log.d(TAG,"googleAPIClient is NULL");
+        }
+        Log.d(TAG,"Leaving onStart");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG,"ConnectionFailed");
+        Toast.makeText(this,"Connection failed",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        loadPermissions(android.Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_FINE_LOCATION);
+        LocationRequest request = LocationRequest.create();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setNumUpdates(1);
+        request.setInterval(0);
+        try{
+            Log.d(TAG,"onConnected reached");
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                    mGoogleApiClient);
+            if (mLastLocation != null) {
+
+                Toast.makeText(this, "Latitude:" + mLastLocation.getLatitude() + ", Longitude:" + mLastLocation.getLongitude(), Toast.LENGTH_LONG).show();
+                Log.d(TAG, "Latitude:" + mLastLocation.getLatitude() + ", Longitude:" + mLastLocation.getLongitude());
+            }else{
+                Log.d(TAG,"mLastLocation is null");
+
+            }
+        }catch(SecurityException ex){
+            Log.d(TAG,"SecurityException ");
+            ex.printStackTrace();
+        }catch(Exception ex){
+            Log.d(TAG,"Exception");
+            ex.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended");
+    }
+
+    private void loadPermissions(String perm,int requestCode) {
+        if (ContextCompat.checkSelfPermission(this, perm) != PackageManager.PERMISSION_GRANTED) {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(this, perm)) {
+                ActivityCompat.requestPermissions(this, new String[]{perm},requestCode);
+            }
+        }
     }
 }
