12d26814ea142b8c33f61b4fadf4c80ddd891171
programmingnat <np7771@yahoo.com>
2016-05-07 12:17:48 -0400

    Added ability to get location in callback

--- LocationServices/app/src/main/java/com/imaginat/locationservices/MainActivity.java
+++ LocationServices/app/src/main/java/com/imaginat/locationservices/MainActivity.java
@@ -1,5 +1,6 @@
 package com.imaginat.locationservices;
 
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -11,14 +12,23 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 public class MainActivity extends AppCompatActivity
- implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+ implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,LocationListener {
 
     public static final String TAG = MainActivity.class.getName();
     private static final int REQUEST_FINE_LOCATION = 0;
+    final static int REQUEST_LOCATION = 199;
+
     GoogleApiClient mGoogleApiClient;
     Location mLastLocation;
     @Override
@@ -45,24 +55,69 @@ public class MainActivity extends AppCompatActivity
         }else{
             Log.d(TAG,"googleAPIClient is NULL");
         }
-        Log.d(TAG,"Leaving onStart");
+        Log.d(TAG, "Leaving onStart");
     }
 
     @Override
+    protected void onStop() {
+
+        if(mGoogleApiClient!=null){
+            mGoogleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //re-factor this code, identical to below
+        final LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        try {
+            if (mGoogleApiClient.isConnected()) {// && !mRequestingLocationUpdates) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(
+                        mGoogleApiClient, mLocationRequest, MainActivity.this);
+                //startLocationUpdates();
+            }
+        }catch(SecurityException se){
+
+        }
+    }
+    @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.d(TAG,"ConnectionFailed");
+        Log.d(TAG, "ConnectionFailed");
         Toast.makeText(this,"Connection failed",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onConnected(Bundle bundle) {
         loadPermissions(android.Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_FINE_LOCATION);
-        LocationRequest request = LocationRequest.create();
-        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        request.setNumUpdates(1);
-        request.setInterval(0);
+//        LocationRequest request = LocationRequest.create();
+//        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+//        request.setNumUpdates(1);
+//        request.setInterval(0);
+
+
         try{
             Log.d(TAG,"onConnected reached");
+
+            createLocationRequest();
+            /*
             mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
                     mGoogleApiClient);
             if (mLastLocation != null) {
@@ -72,7 +127,7 @@ public class MainActivity extends AppCompatActivity
             }else{
                 Log.d(TAG,"mLastLocation is null");
 
-            }
+            }*/
         }catch(SecurityException ex){
             Log.d(TAG,"SecurityException ");
             ex.printStackTrace();
@@ -88,11 +143,71 @@ public class MainActivity extends AppCompatActivity
         Log.d(TAG, "onConnectionSuspended");
     }
 
+
+
+    protected void createLocationRequest() throws SecurityException{
+        final LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient,
+                        builder.build());
+
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) throws SecurityException {
+                final Status status = result.getStatus();
+                //final LocationSettingsStates h = result.getLocationSettingsStates();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        // All location settings are satisfied. The client can
+                        // initialize location requests here.
+                        Log.d(TAG,"Location Setting Requst status code is success");
+                        LocationServices.FusedLocationApi.requestLocationUpdates(
+                                mGoogleApiClient, mLocationRequest, MainActivity.this);
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied, but this can be fixed
+                        // by showing the user a dialog.
+                        Log.d(TAG,"Location Setting Requst status code is resolution required");
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(
+                                    MainActivity.this,
+                                    REQUEST_LOCATION);
+                        } catch (IntentSender.SendIntentException e) {
+                            // Ignore the error.
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        Log.d(TAG,"Location Setting Requst status code is setting change unavaible");
+                        // Location settings are not satisfied. However, we have no way
+                        // to fix the settings so we won't show the dialog.
+
+                        break;
+                }
+            }
+        });
+
+    }
+
     private void loadPermissions(String perm,int requestCode) {
         if (ContextCompat.checkSelfPermission(this, perm) != PackageManager.PERMISSION_GRANTED) {
             if (!ActivityCompat.shouldShowRequestPermissionRationale(this, perm)) {
                 ActivityCompat.requestPermissions(this, new String[]{perm},requestCode);
             }
         }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(this, "Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude(), Toast.LENGTH_LONG).show();
+        Log.d(TAG, "Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
     }
 }
